09531755da69088bcd728bc787bd30101d2306f0
wuzhangpeng <wzhangpeng1989@163.com>
2017-12-27 14:26:43 +0800

    add

--- README.md
+++ README.md
@@ -1,13 +1,10 @@
 # mongodb-study
-mongodb学习，基于node的
+浮点数运算
 
-mongodb教程：https://www.cnblogs.com/best/p/6212807.html   
-测试端口：mongod --port=20300  
+- github地址：https://github.com/MikeMcl/decimal.js、
+- 教程地址：http://www.cnblogs.com/zhongweiv/p/nodejs_decimal.html
+
 
 branch | remarks
 ---|---
-master | 连接mongodb
-test1 | 连接mongodb第二种写法
-test11 | 数据库常用操作
-schedule | 定时任务
 decimal | 浮点数运算
--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-var mongoose = require('mongoose')
-mongoose.Promise = global.Promise;
+var Decimal = require('decimal.js');
 
+// 加法
+var a = 0.1
+var b = 0.2
+console.log('直接加法运算 a + b =', a + b)
+console.log('Decimal.js加法运算 a + b =', new Decimal(a).add(new Decimal(b)).toNumber())
 
-mongoose.connect('mongodb://localhost:20300/test',{useMongoClient:true}).then(
-    () => {
-        console.log("数据库连接成功")
-    },
-    err => {
-        console.log("数据库连接失败")
-    }
-);
+// 减法
+var a = 1.0
+var b = 0.7
+console.log('直接减法运算 a - b =', a - b)
+console.log('Decimal.js减法运算 a - b =', new Decimal(a).sub(new Decimal(b)).toNumber())
+
+// 乘法
+var a = 1.01
+var b = 1.003
+console.log('直接乘法运算 a * b =', a * b)
+console.log('Decimal.js乘法运算 a * b =', new Decimal(a).mul(new Decimal(b)).toNumber())
+
+// 除法
+var a = 0.029
+var b = 10
+console.log('直接除法运算 a / b =', a / b)
+console.log('Decimal.js除法运算 a + b =', new Decimal(a).div(new Decimal(b)).toNumber())
