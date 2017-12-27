d55e5ef950cd91e5032bfbf5010ea4ebd6fa3d82
wuzhangpeng <wzhangpeng1989@163.com>
2017-12-27 11:08:25 +0800

    connect

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # mongodb-study
 mongodb学习，基于node的
+
+mongodb教程：https://www.cnblogs.com/best/p/6212807.html
+测试端口：mongod --port=20300
+branch|remarks
+---|---
+master|连接mongodb
---
+++ app.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose')
+mongoose.Promise = global.Promise;
+
+
+mongoose.connect('mongodb://localhost:20300/test',{useMongoClient:true}).then(
+    () => {
+        console.log("数据库连接成功")
+    },
+    err => {
+        console.log("数据库连接失败")
+    }
+);
